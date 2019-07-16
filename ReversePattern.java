46ffc2782d8f8ca546d8a9a31267b94a89b27ad3
Abhishek Sinha <iamabhisheksinha@gmail.com>
2019-07-17 01:06:04 +0530

    2nd commit

---
+++ ReversePattern.java
@@ -0,0 +1,21 @@
+package abhishek;
+
+public class ReversePattern {
+
+	public static void main(String[] args) {
+		int k=1;
+		for(int i=5;i>=1;--i)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				
+				System.out.print(k+" ");
+				++k;
+			}
+			System.out.println(" ");
+
+		}
+		// TODO Auto-generated constructor stub
+	}
+
+}
